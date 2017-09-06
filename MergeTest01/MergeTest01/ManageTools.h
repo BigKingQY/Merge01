f5d67113cc183fbbaa08e0d51f49446e30360f24
BigKing <641648019@qq.com>
2017-09-06 10:29:01 +0800

    save data

---
+++ MergeTest01/MergeTest01/ManageTools.h
@@ -0,0 +1,15 @@
+//
+//  ManageTools.h
+//  MergeTest01
+//
+//  Created by BigKing on 2017/9/6.
+//  Copyright © 2017年 BigKing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ManageTools : NSObject
+
+- (void)show;
+
+@end
---
+++ MergeTest01/MergeTest01/ManageTools.m
@@ -0,0 +1,17 @@
+//
+//  ManageTools.m
+//  MergeTest01
+//
+//  Created by BigKing on 2017/9/6.
+//  Copyright © 2017年 BigKing. All rights reserved.
+//
+
+#import "ManageTools.h"
+
+@implementation ManageTools
+
+- (void)show{
+    NSLog(@"吾乃merge01");
+}
+
+@end
